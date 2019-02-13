eee732bc0296a22d096e17de51a4e8a38a149847
何平贵 <hepinggui@tinkers.com.cn>
2019-02-13 15:31:22 +0800

    Initial Commit

---
+++ MyIOSEX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIOSEX
+//
+//  Created by Tinkers on 2019/2/13.
+//  Copyright © 2019年 Tinkers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyIOSEXTests/MyIOSEXTests.m
@@ -0,0 +1,37 @@
+//
+//  MyIOSEXTests.m
+//  MyIOSEXTests
+//
+//  Created by Tinkers on 2019/2/13.
+//  Copyright © 2019年 Tinkers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyIOSEXTests : XCTestCase
+
+@end
+
+@implementation MyIOSEXTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
